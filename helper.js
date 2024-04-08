8afd900ea6d6fb1c500208366d9d09ec79a85066
Jaafar Tanoukhi <jafar.tano12@outlook.com>
2024-04-08 15:37:58 +0300

    improved helper function

--- helper.js
+++ helper.js
@@ -1,13 +1,38 @@
-function createModal(component, title, bodyMeta, footerMeta){
-    $A.createComponent('c:CustomModal',{
-        "title" : title,
-        "bodyMeta"  : bodyMeta,
-        "footerMeta" : footerMeta
-    },
-    function(modal,status,errorMessage){
-        if(status === 'SUCCESS') {
-            component.set('v.body',[component.get('v.body'),modal])
+getModal : function (component){
+    return {
+
+        title,
+        message,
+        bodyComponents : [],
+        footerButtons : [{label: 'Cancel', onClick : function(modal){modal.destroy()}}],
+        callback,
+
+        addBodyComponent : function(componentName,componentAttributes){
+            this.bodyComponents.push({componentName : componentName, attributes : componentAttributes});
+        },
+
+        addFooterButton : function(buttonLabel, buttonAction){
+            this.footerButtons.push({label : buttonLabel, onClick : buttonAction})
+        },
+
+        setCallback : function(callback){
+            this.callback = callback;
+        },
+
+        fire : function(){
+            $A.createComponent('c:CustomModal',{
+                "title" : title,
+                "bodyMessage" : message,
+                "bodyMeta"  : this.bodyComponents,
+                "footerMeta" : this.footerButtons
+            },
+            function(modal,status,errorMessage){
+                if(status === 'SUCCESS') {
+                    component.set('v.body',[component.get('v.body'),modal])
+                    this.callback();
+                }
+                else alert(errorMessage);
+            })
         }
-        else alert(errorMessage);
-    })
-}
+        }
+    }
